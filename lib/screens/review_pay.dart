68b605610271993e6c3f5256ebc410b22d84ae87
Lorenzo Dentis <lory9894@gmail.com>
2022-11-28 09:39:34 +0100

    code review & small adjustment by @lory9894

--- lib/screens/review_pay.dart
+++ lib/screens/review_pay.dart
@@ -1,59 +1,46 @@
-import 'dart:convert';
-
 import 'package:command_app_frontend/global.dart';
 import 'package:command_app_frontend/screens/pay_card.dart';
-import 'package:command_app_frontend/screens/shopping_cart.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
 class ReviewPay extends StatelessWidget {
   const ReviewPay({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final total2 = ModalRoute.of(context)!.settings.arguments as double;
-    final total = total2.toString();
     return Scaffold(
-      appBar: AppBar(
-          title: Text("Pagamento")
-      ),
+      appBar: AppBar(title: Text("Pagamento")),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-        const Padding(padding: EdgeInsets.symmetric(vertical: 10)),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text("Totale: $total €"),
-          ],
-        ),
-        const Padding(padding: EdgeInsets.symmetric(vertical: 10)),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const PayCard()),
-                );
-              },
-              child: const Text('Paga con carta'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const PayCard()),
-                );
-              },
-              child: const Text('PayPal'),
-            ),
-          ],
-        ),
-      ],),
-      );
+          const Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text("Totale: ${order.total} €"),
+            ],
+          ),
+          const Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const PayCard()),
+                  );
+                },
+                child: const Text('Paga con carta'),
+              ),
+              ElevatedButton(
+                onPressed:
+                    () {}, //TODO: implement, or maybe not, fuck it, mock it
+                child: const Text('PayPal'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
-
-
