3dbbe8cff1c85bd981184977e0a3b25cd99dc51c
marcooamato <amatomarco111@gmail.com>
2022-11-28 10:29:51 +0100

    DataTable done

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text("title"),
       ),
-      body: Menu()
+      body: PreparationsTable()
     );
   }
 }
--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
 class Dish {
   final String name;
@@ -18,106 +17,223 @@ class Dish {
       required this.course});
 }
 
-class Menu extends StatefulWidget {
-  const Menu({Key? key}) : super(key: key);
+enum PreparationState { ready, underway, waiting }
 
-  @override
-  _MenuState createState() => _MenuState();
+extension PreparationStateStrings on PreparationState {
+  String get str {
+    switch (this) {
+      case PreparationState.waiting:
+        return "In attesa";
+      case PreparationState.ready:
+        return "Pronta";
+      case PreparationState.underway:
+        return "In preparazione";
+    }
+  }
 }
 
-class _MenuState extends State<Menu> {
-  List<Dish> dishes = List.empty(growable: true);
-  Set<String>? courses;
+class Preparation {
+  final Dish dish;
+  final String tableDeliveryCode;
+  PreparationState state;
+
+  Preparation(this.dish, this.tableDeliveryCode,
+      {this.state = PreparationState.underway});
+}
+
+class PreparationsTable extends StatefulWidget {
+  const PreparationsTable({Key? key}) : super(key: key);
+
+  @override
+  _PreparationsTableState createState() => _PreparationsTableState();
+}
+
+class _PreparationsTableState extends State<PreparationsTable> {
+  List<Preparation> preparationsList = List.empty(growable: true);
 
   @override
   void initState() {
-    readJson();
+    preparationsList += getPreparations();
     super.initState();
-  }
-
-  void readJson() async {
-    var json = """ 
-    [
-  {
-    "name": "il Petrone",
-    "price": 5.50,
-    "description": "panino con prosciutto e mozzarella",
-    "imageUrl": "faioeghd",
-    "course": "Panino"
-  },
-  {
-    "name": "Coppo di Vino",
-    "price": 100.00,
-    "description": "vino rosso",
-    "imageUrl": "faioeghd",
-    "course": "Bevanda"
-  },
-    {
-        "name": "CapecchiCola",
-        "price": 100.00,
-        "description": "hoha hola",
-        "imageUrl": null,
-        "course": "Bevanda"
-    }
-
-]
-""";
-    // to make it work https://stackoverflow.com/questions/65630743/how-to-solve-flutter-web-api-cors-error-only-with-dart-code
-    // then python -m http.server 8080 in the folder that contains dummy_dishes.json
-    http.get(Uri.http("localhost:8080", "dummy_dishes.json")).then((value) => {
-          setState(() {
-            dishes = (jsonDecode(value.body) as List)
-                .map((e) => Dish(
-                    name: e["name"],
-                    description: e["description"],
-                    price: e["price"],
-                    imageUrl: e["imageUrl"],
-                    course: e["course"]))
-                .toList();
-            courses = dishes.map((e) => e.course).toSet();
-          }),
-        });
-  }
-
-  //if the list of courses is not provided, it will be generated from the list of dishes
-  Set<String> _dishes_courses() {
-    courses ??= dishes.map((dish) => dish.course).toSet();
-    return courses!;
   }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      //find the number of different courses in list dishes
-
-      itemCount: _dishes_courses().length,
-      itemBuilder: (BuildContext context, int index) {
-        return ExpansionTile(
-          //title : course name white text
-          title: Text(
-            _dishes_courses().elementAt(index),
-          ),
-          //background color of the tile when closed
-          collapsedBackgroundColor: Theme.of(context).backgroundColor,
-
-          children: dishes
-              .where(
-                  (dish) => dish.course == _dishes_courses().elementAt(index))
-              .map((dish) => ListTile(
-                    tileColor: Colors.white,
-                    leading: CircleAvatar(
-                      backgroundImage: dish.imageUrl != null
-                          ? NetworkImage(dish.imageUrl!)
-                          : null,
-                      child: dish.imageUrl == null ? Text(dish.name[0]) : null,
+    // TODO rendere le righe una reordable list
+    return Container(
+        padding: const EdgeInsets.all(0),
+        width: double.infinity,
+        height: double.infinity,
+        alignment: Alignment.topCenter,
+        child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: DataTable(
+                columnSpacing: 12,
+                horizontalMargin: 12,
+                columns: const <DataColumn>[
+                  DataColumn(
+                      label: Expanded(
+                    child: Text(
+                      'Preparazione',
+                      style: TextStyle(fontStyle: FontStyle.italic),
                     ),
-                    title: Text(dish.name),
-                    subtitle: Text("${dish.description} - ${dish.price}€"),
-                    trailing: const Icon(Icons.add),
-                  ))
-              .toList(),
-        );
-      },
+                  )),
+                  DataColumn(
+                      label: Expanded(
+                    child: Text(
+                      'Codice',
+                      style: TextStyle(fontStyle: FontStyle.italic),
+                    ),
+                  )),
+                  DataColumn(
+                      label: Expanded(
+                    child: Text(
+                      'Stato',
+                      style: TextStyle(fontStyle: FontStyle.italic),
+                    ),
+                  )),
+                  DataColumn(
+                      label: Expanded(
+                    child: Text(
+                      'Cambia Stato',
+                      style: TextStyle(fontStyle: FontStyle.italic),
+                    ),
+                  )),
+                ],
+                rows: List<DataRow>.generate(
+                    preparationsList.length,
+                    (index) => DataRow(cells: [
+                          DataCell(
+                              Text(preparationsList[index].dish.name)),
+                          DataCell(
+                              Text(preparationsList[index].tableDeliveryCode)),
+                          DataCell(Text(preparationsList[index].state.str)),
+                          DataCell(Row(
+                            children: const [
+                              IconButton(
+                                  onPressed: null,
+                                  icon: Icon(Icons.account_balance)),
+                              IconButton(
+                                  onPressed: null,
+                                  icon: Icon(Icons.account_balance)),
+                              IconButton(
+                                  onPressed: null,
+                                  icon: Icon(Icons.account_balance)),
+                            ],
+                          )),
+                        ]))))
     );
+
+    // throw UnimplementedError();
+
+    // return Table(
+    //     border: TableBorder.all(),
+    //     columnWidths: const <int, TableColumnWidth>{
+    //       0: FractionColumnWidth(35),
+    //       1: FractionColumnWidth(10),
+    //       2: FractionColumnWidth(20),
+    //       3: FractionColumnWidth(35),
+    //       // continua da qui, devi mettere i dati ma le dimensioni delle colonne sono giuste (credo)
+    //     },
+    //     defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+    //     children: <TableRow>[
+    //       TableRow(
+    //         children: <Widget>[
+    //           Container(
+    //             height: double.infinity,
+    //             color: Colors.green,
+    //           ),
+    //           TableCell(
+    //             verticalAlignment: TableCellVerticalAlignment.top,
+    //             child: Container(
+    //               height: double.infinity,
+    //               color: Colors.red,
+    //             ),
+    //           ),
+    //           Container(
+    //             height: double.infinity,
+    //             color: Colors.blue,
+    //           ),
+    //           Container(
+    //             color: Colors.blue,
+    //           ),
+    //         ],
+    //       ),
+    //       TableRow(
+    //         decoration: const BoxDecoration(
+    //           color: Colors.grey,
+    //         ),
+    //         children: <Widget>[
+    //           Container(
+    //             color: Colors.purple,
+    //           ),
+    //           Container(
+    //             color: Colors.yellow,
+    //           ),
+    //           Container(
+    //             color: Colors.yellow,
+    //           ),
+    //           Center(
+    //             child: Container(
+    //               color: Colors.orange,
+    //             ),
+    //           ),
+    //         ],
+    //       ),
+    //     ],
+    //   );
+
+    // return ListView.builder(
+    //   //find the number of different courses in list dishes
+    //
+    //   itemCount: _dishes_courses().length,
+    //   itemBuilder: (BuildContext context, int index) {
+    //     return ExpansionTile(
+    //       //title : course name white text
+    //       title: Text(
+    //         _dishes_courses().elementAt(index),
+    //       ),
+    //       //background color of the tile when closed
+    //       collapsedBackgroundColor: Theme.of(context).backgroundColor,
+    //
+    //       children: dishes
+    //           .where(
+    //               (dish) => dish.course == _dishes_courses().elementAt(index))
+    //           .map((dish) => ListTile(
+    //                 tileColor: Colors.white,
+    //                 leading: CircleAvatar(
+    //                   backgroundImage: dish.imageUrl != null
+    //                       ? NetworkImage(dish.imageUrl!)
+    //                       : null,
+    //                   child: dish.imageUrl == null ? Text(dish.name[0]) : null,
+    //                 ),
+    //                 title: Text(dish.name),
+    //                 subtitle: Text("${dish.description} - ${dish.price}€"),
+    //                 trailing: const Icon(Icons.add),
+    //               ))
+    //           .toList(),
+    //     );
+    //   },
+    // );
+  }
+
+  /// returns list of preparations to show on screen
+  List<Preparation> getPreparations() {
+    // TODO implement reading json and parsing to preparations
+    // dummy implementation
+    Dish dish1 = Dish(
+        name: "Il Petrone",
+        description: "mi piace il panino di Baldoni gnam",
+        price: 6.66,
+        course: "panino");
+    Dish dish2 = Dish(
+        name: "Risotto alla CMRO",
+        description: "Gvosso risotto per buongustai (Fakemuscles approves)",
+        price: 1.25,
+        course: "primo");
+    Preparation preparation1 = Preparation(dish1, "1");
+    Preparation preparation2 =
+        Preparation(dish2, "A46", state: PreparationState.waiting);
+    return [preparation1, preparation2];
   }
 }
