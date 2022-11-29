ba9b16bdc575ba6760d72f1718750ee7e97635b2
marcooamato <amatomarco111@gmail.com>
2022-11-29 20:48:39 +0100

    admin homepage with component (to refactor)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:command_app_frontend/screens/menu.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,28 +15,71 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: const AdminHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class AdminHomePage extends StatelessWidget {
+  const AdminHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    return const MultipleButtonsPage([
+      "Area prenotazioni",
+      "Area cucina",
+      "Area servizio",
+      "Area pinocchio"
+    ]);
+  }
+}
 
+class MultipleButtonsPage extends StatefulWidget{
+  const MultipleButtonsPage(this.buttonsTextList, {super.key});
+
+  final List<String> buttonsTextList;
+
+  @override
+  _MultipleButtonsPageState createState() => _MultipleButtonsPageState();
+
+}
+
+
+class _MultipleButtonsPageState extends State<MultipleButtonsPage> {
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("title"),
+        title: const Text("Comandapp - zona admin"),
       ),
-      body: PreparationsTable()
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: getButtonsWidgets()
+        ),
+      ),
     );
+  }
+
+  List<Widget> getButtonsWidgets(){
+    List<Widget> buttonsWidgets = List.empty(growable: true);
+    for (String buttonText in widget.buttonsTextList) {
+      Widget buttonWidget = SizedBox(
+        width: 300,
+        height: 150,
+        child: ElevatedButton(
+          onPressed: null,
+          child: Text(
+            buttonText,
+            style: const TextStyle(fontSize: 30),
+          ),
+        ),
+      );
+      buttonsWidgets.add(buttonWidget);
+      buttonsWidgets.add(const SizedBox(height: 10));
+    }
+    return buttonsWidgets;
   }
 }
