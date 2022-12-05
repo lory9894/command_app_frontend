bc9075617743ab46a85fef7a3117760d5b924b74
Lorenzo Dentis <lory9894@gmail.com>
2022-12-05 16:54:05 +0100

    fixed scrollable login

--- cliente/lib/screens/login.dart
+++ cliente/lib/screens/login.dart
@@ -20,68 +20,70 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10.0),
-              child: Center(
-                child: SizedBox(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              children: <Widget>[
+                Flex(direction: Axis.vertical, children: [
+                  Image.asset(
+                    'assets/images/ComandApp-circle-noslogan.png',
+                    height: 200,
+                    width: 200,
+                    fit: BoxFit.fitWidth,
+                  )
+                ]),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 5),
+                  child: Container(
+                    height: 300,
                     width: 300,
-                    height: 250,
-                    child: Image.asset(
-                      'assets/images/ComandApp-circle-noslogan.png',
-                      height: 400,
-                      width: 400,
-                      fit: BoxFit.fitWidth,
-                    )), //TODO: Sistemare immagine
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 5),
-              child: Container(
-                height: 400,
-                width: 400,
-                child: MobileScanner(
-                    allowDuplicates: false,
-                    onDetect: (barcode, args) {
-                      if (barcode.rawValue != null) {
-                        final String code = barcode.rawValue!;
-                        debugPrint('Barcode found: $code');
-                        order.tableID = code;
-                        Navigator.of(context).pushReplacement(
-                          MaterialPageRoute(builder: (context) => const Menu()),
-                        );
-                      }
-                    }),
-              ),
-            ),
-            const Padding(padding: EdgeInsets.only(top: 15)),
-            Container(
-              height: 50,
-              width: 250,
-              decoration: BoxDecoration(
-                  color: Colors.black, borderRadius: BorderRadius.circular(20)),
-              child: SignInButton(
-                Buttons.GoogleDark,
-                text: "Log in con Google",
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                onPressed: () async {
-                  //TODO: Autenticazione con google
-                  Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (context) => const Profile()),
-                  );
-                },
-              ),
-            ),
-            const Padding(padding: EdgeInsets.only(top: 30)),
-            const Text('vuoi registrarti?'),
-            TextButton(
-              onPressed: () {},
-              child: const Text(
-                'clicca qui',
-                style: TextStyle(color: Colors.blue, fontSize: 15),
-              ),
+                    child: MobileScanner(
+                        allowDuplicates: false,
+                        onDetect: (barcode, args) {
+                          if (barcode.rawValue != null) {
+                            final String code = barcode.rawValue!;
+                            debugPrint('Barcode found: $code');
+                            order.tableID = code;
+                            Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                  builder: (context) => const Menu()),
+                            );
+                          }
+                        }),
+                  ),
+                ),
+                const Padding(padding: EdgeInsets.only(top: 15)),
+                Container(
+                  height: 50,
+                  width: 250,
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(20)),
+                  child: SignInButton(
+                    Buttons.GoogleDark,
+                    text: "Log in con Google",
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    onPressed: () async {
+                      //TODO: Autenticazione con google
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(
+                            builder: (context) => const Profile()),
+                      );
+                    },
+                  ),
+                ),
+                const Padding(padding: EdgeInsets.only(top: 30)),
+                const Text('vuoi registrarti?'),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'clicca qui',
+                    style: TextStyle(color: Colors.blue, fontSize: 15),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
