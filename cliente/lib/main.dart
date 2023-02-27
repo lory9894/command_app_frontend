a1d814b96bc30a723d3d93209755a3dc9e00bfdd
marcooamato <amatomarco111@gmail.com>
2023-02-27 17:50:00 +0100

    Login as first page again

--- cliente/lib/main.dart
+++ cliente/lib/main.dart
@@ -1,6 +1,6 @@
 // app color palette: Orange (#ffb238), Green (#448a66)
 
-import 'package:command_app_frontend/screens/home_page.dart';
+import 'package:command_app_frontend/screens/login.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -47,7 +47,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // return const Login();
-    return const HomePage();
+    return const LoginPage();
   }
 }
