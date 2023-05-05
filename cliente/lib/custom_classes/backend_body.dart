ea0d399c5b147e5fd6db028a27d74e5babaf7680
Lorenzo Dentis <lory9894@gmail.com>
2023-05-05 16:31:55 +0200

    orderState or state should be consistent

--- cliente/lib/custom_classes/backend_body.dart
+++ cliente/lib/custom_classes/backend_body.dart
@@ -71,7 +71,7 @@ class MessageOrder {
         'dateTime': DateFormat("dd-MM-yyyy HH:mm:ss").format(dateTime),
         'paymentState': paymentState.toString().split(".").last,
         'paymentType': paymentType.toString().split(".").last,
-        'state': orderState.toString().split(".").last,
+        'orderState': orderState.toString().split(".").last,
         'orderType': orderType.toString().split(".").last,
         'total': total,
         'user': user == null
@@ -105,7 +105,7 @@ class Preparation {
     return {
       'name': name,
       'tableNum': tableNum,
-      'orderState': orderState.toString().split(".").last
+      'state': orderState.toString().split(".").last
     };
   }
 }
